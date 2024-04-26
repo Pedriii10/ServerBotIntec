c37eea563801005c881a2c745f79970ca9d85f83
Fernando Nevot <91965836+Fer457@users.noreply.github.com>
2024-04-26 10:35:48 +0200

    healthcheck

--- src/server.js
+++ src/server.js
@@ -18,6 +18,20 @@ const io = new Server(server, {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo va mal");
+});
+
 socketConnection(io);
 
 server.listen(3001, () => console.log("Server started"));
